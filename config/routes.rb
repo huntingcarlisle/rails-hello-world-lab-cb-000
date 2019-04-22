a779405c09bcf197a3442aaf9933a5c625a3cdda
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-04-22 23:09:49 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'hello_world', do: 
+  get 'hello_world', to: 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
